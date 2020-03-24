f8fde911d6bad4ef913099fe554ca35f7b0dcd70
Audran Doublet <audran.doublet@epita.fr>
2020-03-24 22:20:15 +0100

    scene: load solids

--- scene/src/scene.rs
+++ scene/src/scene.rs
@@ -1,3 +1,8 @@
+extern crate serde_yaml;
+
+use std::fs::File;
+use std::path::Path;
+
 use sph_common::{RigidObject};
 
 use serde_derive::*;
@@ -21,7 +26,7 @@ impl Default for Configuration
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 pub struct CommandLineConfiguration
 {
     pub data_path: String,
@@ -29,10 +34,20 @@ pub struct CommandLineConfiguration
     pub use_cache: bool,
 }
 
+impl Default for CommandLineConfiguration {
+    fn default() -> CommandLineConfiguration {
+        CommandLineConfiguration {
+            data_path: "data/".to_string(),
+            cache_path: "cache/".to_string(),
+            use_cache: true,
+        }
+    }
+}
+
 #[derive(Debug, Deserialize)]
 pub struct Scene
 {
-    #[serde(skip_serializing)]
+    #[serde(skip_deserializing)]
     pub global_config: CommandLineConfiguration,
     pub config: Configuration,
     pub solids: Vec<Solid>,
@@ -40,15 +55,28 @@ pub struct Scene
 
 impl Scene
 {
-    pub fn load_solids(&self) -> Vec<RigidObject>
+    pub fn create_cache_dir(&self) -> Result<(), Box<dyn std::error::Error>> {
+        std::fs::create_dir_all(Path::new(&self.global_config.cache_path))?;
+        Ok(())
+    }
+
+    pub fn load_solids(&self) -> Result<Vec<RigidObject>, Box<dyn std::error::Error>>
     {
+        self.create_cache_dir()?;
+
         let mut solids = Vec::new();
 
         for solid in &self.solids
         {
-            solids.push(solid.load(&self));
+            solids.push(solid.load(&self)?);
         }
 
-        solids
+        Ok(solids)
     }
+}
+
+pub fn load_scene(name: &str) -> Result<Scene, Box<dyn std::error::Error>> {
+    let result: Scene = serde_yaml::from_reader(&File::open(name)?)?;
+
+    Ok(result)
 }
--- scene/src/solid.rs
+++ scene/src/solid.rs
@@ -62,19 +62,26 @@ impl Solid {
         cache_path.join(cache_file)
     }
 
-    pub fn load(&self, scene: &Scene) -> RigidObject {
+    pub fn load(&self, scene: &Scene) -> Result<RigidObject, Box<dyn std::error::Error>> {
         let kradius = scene.config.kernel_radius;
 
         let cache_file = self.cache_file(Path::new(&scene.global_config.cache_path), kradius);
         let mesh_file = self.file(Path::new(&scene.global_config.data_path));
 
         let grid = if scene.global_config.use_cache && cache_file.as_path().exists() {
-            let file = File::open(&cache_file).expect("cannot open cache");
-            DiscreteGrid::load(&file).expect("cache reading failed")
+            println!("Use cache {:?} for {}", cache_file, self.mesh);
+
+            let file = File::open(&cache_file)?;
+            DiscreteGrid::load(&file)?
         } else {
+            println!("Compute sdf and volume for {}", self.mesh);
             let extend = 2. * Vector3::new(kradius, kradius, kradius);
 
-            let mesh = Mesh::load_obj(&mesh_file).expect(format!("cannot load mesh {:?}", mesh_file).as_str());
+            let mut mesh = Mesh::load_obj(&mesh_file)?;
+
+            if self.mesh_invert {
+                mesh.invert();
+            }
 
             let mut grid = DiscreteGrid::new(
                 mesh.bounding_min - extend,
@@ -85,14 +92,20 @@ impl Solid {
             mesh.compute_sdf(&mut grid);
             mesh.compute_volume(&mut grid, &CubicSpine::new(kradius));
 
+            println!("Save discretegrid into {:?}", cache_file);
+            let file = File::create(&cache_file)?;
+            grid.save(&file)?;
+
             grid
         };
+
+        println!("{} loaded!", self.mesh);
 
         let mut object = RigidObject::new(grid);
 
         object.set_scale(self.scale());
         object.set_position(self.position());
 
-        object
+        Ok(object)
     }
 }
