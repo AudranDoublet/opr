0ab3f8f6589b41352022b4b82c42cb23c6c3ab21
Issaadi Sami <samiisd@hotmail.fr>
2020-04-07 12:57:54 +0200

    add Prev/Next buttons to the viewer

--- src/viewer.rs
+++ src/viewer.rs
@@ -1,9 +1,10 @@
-use clap::ArgMatches;
-use kiss3d::camera::camera::Camera;
-use nalgebra::{Point3, Vector3};
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
+
+use clap::ArgMatches;
+use kiss3d::camera::camera::Camera;
+use nalgebra::{Point3, Vector3};
 
 fn get_meshes_paths(folder: &Path) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
     let mut files: Vec<PathBuf> = fs::read_dir(folder)?
@@ -40,9 +41,12 @@ fn view_meshes(meshes_folder: &Path, back_face_culling: bool) -> Result<(), Box<
 
     let mut time_step = 0.;
 
+    let mut dt_i_obj: isize = 0;
+
     while renderer.render() {
-        if !pause && now.elapsed().as_secs_f32() > time_step {
-            i_obj = (i_obj + 1) % meshes.len();
+        if !pause && now.elapsed().as_secs_f32() > time_step || dt_i_obj != 0 {
+            i_obj = ((if dt_i_obj == 0 { i_obj as isize + 1 } else { i_obj as isize + dt_i_obj } + meshes.len() as isize) % meshes.len() as isize) as usize; // FIXME: beeeuuuuuuuuurk
+            dt_i_obj = 0;
             renderer.window.remove_node(&mut obj);
             obj = renderer.window.add_obj(&meshes[i_obj], meshes_folder, scale);
             obj.enable_backface_culling(back_face_culling);
@@ -69,13 +73,24 @@ fn view_meshes(meshes_folder: &Path, back_face_culling: bool) -> Result<(), Box<
                         _ => {}
                     }
                 }
+                render::event::WindowEvent::Key(key, render::event::Action::Press, _) => {
+                    match key {
+                        render::event::Key::F => {
+                            dt_i_obj = 1;
+                        }
+                        render::event::Key::S => {
+                            dt_i_obj = -1;
+                        }
+                        _ => {}
+                    }
+                }
                 _ => {}
             }
         }
 
         if show_info {
             renderer.debug_text(&format!("\
-                iteration: {}/{}\n
+                iteration: {}/{}\n\
                 dt: {:.3}\n\
                 eye: {}\
                 ", i_obj, meshes.len() - 1, time_step, renderer.camera.eye()));
