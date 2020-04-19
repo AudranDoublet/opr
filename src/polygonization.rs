066ae642adf4ca13d7efeaa05b948433156cb63a
Issaadi Sami <samiisd@hotmail.fr>
2020-04-19 18:33:23 +0200

    set correct hyper parameters for aniso

--- src/polygonization.rs
+++ src/polygonization.rs
@@ -147,12 +147,12 @@ pub fn main_polygonization(args: &ArgMatches) -> Result<(), Box<dyn std::error::
 
     let anisotropicator = if !disable_anisotropication {
         Some(
-            Anisotropicator::new(0.9, 15, 40., 20000., 50.)
+            Anisotropicator::new(1., 25, 4., 20000., 50.)
         )
     } else { None };
 
     // FIXME: the ISO-VALUE and CUBE-SIZE should be asked in CLI instead of being hardcoded
-    let mesher = Mesher::new(0.00001, 0.05, interpolation_algorithm, anisotropicator);
+    let mesher = Mesher::new(0.00001, 0.08, interpolation_algorithm, anisotropicator);
     // FIXME-END
 
     fs::create_dir_all(output_directory)?;
