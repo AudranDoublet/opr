d7e9c0f49c5c121ca4f2104b1188d9bd17f7923e
Issaadi Sami <samiisd@hotmail.fr>
2020-04-20 22:42:09 +0200

    fix CubicSpine `unormalized_apply_on_norm` implem -> fix aniso

--- common/src/kernel.rs
+++ common/src/kernel.rs
@@ -7,7 +7,6 @@ pub trait Kernel {
     fn apply_on_norm(&self, r_norm: f32) -> f32;
     fn gradient(&self, r: &Vector3<f32>) -> Vector3<f32>;
     fn unormalized_apply_on_norm(&self, r_norm: f32, normalizer: f32) -> f32;
-    fn unormalized_gradient(&self, r: &Vector3<f32>, normalizer: f32) -> Vector3<f32>;
 
     fn radius(&self) -> f32;
     fn radius_sq(&self) -> f32;
@@ -39,31 +38,25 @@ pub mod kernels {
 
     impl Kernel for CubicSpine {
         fn apply_on_norm(&self, r_norm: f32) -> f32 {
-            self.unormalized_apply_on_norm(r_norm, self.normalizer)
+            self.unormalized_apply_on_norm(r_norm / self.h, self.normalizer)
         }
 
         fn gradient(&self, r: &Vector3<f32>) -> Vector3<f32> {
-            self.unormalized_gradient(r, self.normalizer)
+            let r_norm = r.norm();
+            let q = r_norm / self.h;
+
+            6. * self.sigma * self.normalizer * r * (1. / (r_norm * self.h)) * match q {
+                q if r_norm <= 1e-5 || q > 1.0 => 0.0,
+                q if q <= 0.5 => q * (3. * q - 2.),
+                q => -(1. - q).powi(2),
+            }
         }
 
-        fn unormalized_apply_on_norm(&self, r_norm: f32, normalizer: f32) -> f32 {
-            let q: f32 = r_norm / self.h;
-
+        fn unormalized_apply_on_norm(&self, q: f32, normalizer: f32) -> f32 {
             self.sigma * normalizer * match q {
                 q if q <= 0.5 => 6. * (q.powi(3) - q.powi(2)) + 1.,
                 q if q <= 1.0 => 2. * (1. - q).powi(3),
                 _ => 0.0
-            }
-        }
-
-        fn unormalized_gradient(&self, r: &Vector3<f32>, normalizer: f32) -> Vector3<f32> {
-            let r_norm = r.norm();
-            let q = r_norm / self.h;
-
-            6. * self.sigma * normalizer * r * (1. / (r_norm * self.h)) * match q {
-                q if r_norm <= 1e-5 || q > 1.0 => 0.0,
-                q if q <= 0.5 => q * (3. * q - 2.),
-                q => -(1. - q).powi(2),
             }
         }
 
